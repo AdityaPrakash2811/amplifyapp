e019c53984e06bd827295725fdfe07cfef668ea5
AdityaPrakash2811 <adityaprakash.2811@gmail.com>
2020-11-12 12:47:22 +0530

    added storage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 //Added Later for api
 import React, { useState, useEffect } from 'react';
-import { API } from 'aws-amplify';
+import { API, /*for Storage */Storage } from 'aws-amplify';
 import { listTodos } from './graphql/queries';
 import { createTodo as createTodoMutation, deleteTodo as deleteTodoMutation } from './graphql/mutations';
 
@@ -22,14 +22,39 @@ function App() {
     fetchNotes();
   }, []);
 
+  //for Storage
+  async function onChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+    fetchNotes();
+  }
+
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listTodos });
+    //storage...checking if todo is asso. with an img
+    const todosFromAPI = apiData.data.listTodos.items;
+    await Promise.all(todosFromAPI.map(async todo => {
+      if (todo.image) {
+        const image = await Storage.get(todo.image);
+        todo.image = image;
+      }
+      return todo;
+    }))
+    //end storage code
     setNotes(apiData.data.listTodos.items);
   }
 
   async function createTodo() {
     if (!formData.name || !formData.description) return;
     await API.graphql({ query: createTodoMutation, variables: { input: formData } });
+    //Storage code
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
+    //end storage code
     setNotes([ ...notes, formData ]);
     setFormData(initialFormState);
   }
@@ -49,10 +74,14 @@ function App() {
         placeholder="Note name"
         value={formData.name}
       /><br/><br/>
-      <input
+      <textarea
         onChange={e => setFormData({ ...formData, 'description': e.target.value})}
         placeholder="Note description"
         value={formData.description}
+      /><br/><br/>
+      <input
+        type="file"
+        onChange={onChange}
       /><br/><br/>
       <button onClick={createTodo}>Create Note</button><br/><br/>
       <div style={{marginBottom: 30}}>
@@ -61,6 +90,9 @@ function App() {
             <div key={note.id || note.name}>
               <h2>{note.name}</h2>
               <p>{note.description}</p>
+              {
+                note.image && <img src={note.image} style={{width: 400}} />
+              }
               <button onClick={() => deleteTodo(note)}>Delete note</button>
             </div>
           ))
